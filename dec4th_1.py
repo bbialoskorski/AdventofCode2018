42dcf955b8dadb78bda8e5b0be576b408b397c75
bbialoskorski <bartoszbialoskorski@gmail.com>
2018-12-04 14:03:41 +0100

    dec4th

---
+++ dec4th_1.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+import operator
+import sys
+
+date_format = '%Y-%m-%d %H:%M'
+
+def sol():
+
+    records = sys.stdin.readlines()
+
+    events = list()
+
+    for record in records:
+
+        timestamp = record.split(']')[0].replace('[', '')
+        event = record.split(']')[1]
+        event = event[1:len(event) - 1]
+
+        events.append((timestamp, event))
+
+    events.sort(key=lambda event: event[0])
+
+    minute_stats = dict()
+    minutes_slept = dict()
+
+    fell_asleep = 0
+    guard_id = 0
+
+    for event in events: 
+
+        if event[1][0] == 'G':
+
+            guard_id = int(event[1].split(' ')[1][1:])
+
+        if event[1][0] == 'f':
+
+            fell_asleep = datetime.strptime(event[0], date_format).minute
+
+        if event[1][0] == 'w':
+
+            woke_up = datetime.strptime(event[0], date_format).minute
+
+            if guard_id not in minutes_slept:
+
+                minutes_slept[guard_id] = 0
+                minute_stats[guard_id] = list()
+
+                for i in range(60):
+
+                    minute_stats[guard_id].append(0)
+                
+            minutes_slept[guard_id] += woke_up - fell_asleep
+
+            for minute in range(fell_asleep, woke_up, 1):
+                
+               minute_stats[guard_id][minute] += 1 
+
+    laziest = max(minutes_slept.items(), key=operator.itemgetter(1))[0]
+
+    minute = minute_stats[laziest].index(max(minute_stats[laziest]))
+
+    return laziest * minute
+
+print(sol())
---
+++ dec4th_2.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+import operator
+import sys
+
+date_format = '%Y-%m-%d %H:%M'
+
+def sol():
+
+    records = sys.stdin.readlines()
+
+    events = list()
+
+    for record in records:
+
+        timestamp = record.split(']')[0].replace('[', '')
+        event = record.split(']')[1]
+        event = event[1:len(event) - 1]
+
+        events.append((timestamp, event))
+
+    events.sort(key=lambda event: event[0])
+
+    minute_stats = dict()
+
+    fell_asleep = 0
+    guard_id = 0
+
+    for event in events: 
+
+        if event[1][0] == 'G':
+
+            guard_id = int(event[1].split(' ')[1][1:])
+
+        if event[1][0] == 'f':
+
+            fell_asleep = datetime.strptime(event[0], date_format).minute
+
+        if event[1][0] == 'w':
+
+            woke_up = datetime.strptime(event[0], date_format).minute
+
+            if guard_id not in minute_stats:
+
+                minute_stats[guard_id] = list()
+
+                for i in range(60):
+
+                    minute_stats[guard_id].append(0)
+                
+
+            for minute in range(fell_asleep, woke_up, 1):
+                
+               minute_stats[guard_id][minute] += 1 
+
+    max_id = 0
+    max_minute = 0
+    max_frequency = 0
+
+    for i in range(60):
+
+        local_max_id = 0
+        local_max_frequency = 0
+
+        for guard in minute_stats.items():
+
+            if guard[1][i] > local_max_frequency:
+
+                local_max_frequency = guard[1][i]
+                local_max_id = guard[0]
+
+        if local_max_frequency > max_frequency:
+
+            max_frequency = local_max_frequency
+            max_minute = i
+            max_id = local_max_id
+    
+    print(max_id, max_minute)
+
+    return max_id * max_minute
+
+print(sol())
